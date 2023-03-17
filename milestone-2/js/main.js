41eb83874da35d88c8725d2ec8058c3f24721097
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-17 11:46:07 +0100

    Visualizzazione dinamica messaggi

--- milestone-2/js/main.js
+++ milestone-2/js/main.js
@@ -5,6 +5,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      userSelected: 0,
       contacts: [
         {
           name: "Michele",
@@ -14,17 +15,17 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Hai portato a spasso il cane?",
-              status: "sent",
+              status: 'sent',
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Ricordati di stendere i panni",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 16:15:22",
               message: "Tutto fatto!",
-              status: "received",
+              status: false,
             },
           ],
         },
@@ -36,17 +37,17 @@ createApp({
             {
               date: "20/03/2020 16:30:00",
               message: "Ciao come stai?",
-              status: "sent",
+              status: true,
             },
             {
               date: "20/03/2020 16:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
-              status: "received",
+              status: false,
             },
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent",
+              status: true,
             },
           ],
         },
@@ -58,17 +59,17 @@ createApp({
             {
               date: "28/03/2020 10:10:40",
               message: "La Marianna va in campagna",
-              status: "received",
+              status: false,
             },
             {
               date: "28/03/2020 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
-              status: "sent",
+              status: true,
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
-              status: "received",
+              status: false,
             },
           ],
         },
@@ -80,12 +81,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Si, ma preferirei andare al cinema",
-              status: "received",
+              status: false,
             },
           ],
         },
@@ -97,12 +98,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ricordati di chiamare la nonna",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Va bene, stasera la sento",
-              status: "received",
+              status: false,
             },
           ],
         },
@@ -114,17 +115,17 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao Claudia, hai novità?",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Non ancora",
-              status: "received",
+              status: false,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "Nessuna nuova, buona nuova",
-              status: "sent",
+              status: true,
             },
           ],
         },
@@ -136,12 +137,12 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
-              status: "received",
+              status: false,
             },
           ],
         },
@@ -153,17 +154,17 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
-              status: "received",
+              status: false,
             },
             {
               date: "10/01/2020 15:50:00",
               message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
-              status: "sent",
+              status: true,
             },
             {
               date: "10/01/2020 15:51:00",
               message: "OK!!",
-              status: "received",
+              status: false,
             },
           ],
         },
